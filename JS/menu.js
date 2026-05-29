eaf0c188dbd3549ade785d70f26cf4907f20c71b
Santiago Achenapure <santiagofile@gmail.com>
2026-05-29 08:58:49 -0300

    Fixed .fantasma

--- JS/menu.js
+++ JS/menu.js
@@ -1,5 +1,7 @@
 const primaryNav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".menu-btn");
+const CloseAll = document.querySelector(".fantasma");
+
 
 navToggle.addEventListener("click", () => {
    
@@ -9,8 +11,23 @@ navToggle.addEventListener("click", () => {
     if (visibility === "true") {
         primaryNav.setAttribute("data-visible", "false");
         navToggle.setAttribute("aria-expanded", "false"); 
+        CloseAll.setAttribute("data-visible", "false");
     } else if (visibility === "false") {
         primaryNav.setAttribute("data-visible", "true");
         navToggle.setAttribute("aria-expanded", "true");
+        CloseAll.setAttribute("data-visible", "true");
     }
+
+});
+
+CloseAll.addEventListener ('click', () => {
+    const visibility = primaryNav.getAttribute('data-visible');
+
+if (visibility === "true") {
+        primaryNav.setAttribute("data-visible", "false");
+        navToggle.setAttribute("aria-expanded", "fasle");
+        CloseAll.setAttribute("data-visible", "false");
+    
+    }
+
 });
