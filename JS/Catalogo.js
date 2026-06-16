be6f23ddfe99c6a83ced7a345f2bb3b2debd90ec
Santiago Achenapure <santiagofile@gmail.com>
2026-06-15 23:10:19 -0300

    lista de favoritos

--- JS/Catalogo.js
+++ JS/Catalogo.js
@@ -9,11 +9,23 @@ const contenedorCatalogo = document.querySelector('.contenedor-productos');
   //Variable lista fav
 let favoritos = JSON.parse(localStorage.getItem('favoritos')) || []
 
+
+//llamar a los elementos del DOM
+const contenedotCatalogo = document.querySelector('.contenedor-catalogo');
+
+//Construir la lista de favoritos
+const ListaFavoritos = document.querySelector('.lista-favoritos');
+
+//contruir contador de Favoritos
+const favCount = document.querySelector('.fav-count');
+
+//Carga produtos desde el .json
   async function cargarProductos() {
     try{
         const res = await fetch('DATA/producto.json')
         productosHtech = await res.json(); //similar a JSON.parse()
-        renderizarCatalogo(); 
+        renderizarCatalogo();
+        renderizarFavoritos();
     }catch(error){
         console.error('error al cargar los productos', error);
     }
@@ -35,7 +47,7 @@ const catalogo = productosHtech.map(producto =>{
             <h2>${producto.titulo}</h2>
             <p class="precio">$${producto.precio.toLocaleString('es-AR')}</p>
 
-            <button class="btn-favorito ${esFavorito ? 'active' : ''}" data-id="${producto.id}">
+            <button class="btn-AddFavorito ${esFavorito ? 'active' : ''}" data-id="${producto.id}">
                         <span class="sr-only">${esFavorito ? 'en favoritos' : 'Agregar a favoritos'}</span>
                     </button>
             <button class="btn-agregar-carrito">Agregar al carrito</button>
@@ -45,21 +57,71 @@ const catalogo = productosHtech.map(producto =>{
     contenedorCatalogo.innerHTML = catalogo;
 }
 
+contenedorCatalogo.addEventListener('click', (e) => {
+    if (e.target.closest('.btn-AddFavorito')) {
+        toggleFavorito(e);
+    }
+});
+
 //togle favoritos
 function toggleFavorito(e) {
     const id = Number(e.target.dataset.id);
     if (favoritos.includes(id)) {
     favoritos = favoritos.filter(favID => favID !== id);
     e.target.classList.remove('active');
-    e.target.textContent = 'Agregar a favoritos';
+     e.target.innetHTML ='<span class="sr-only">Agregar a Favoritos<span/>';
 } else {
     favoritos.push(id);
     e.target.classList.add('active');
-    e.target.textContent = 'en favoritos';
+    e.target.innetHTML ='<span class="sr-only">En Favoritos<span/>';
 
 }
 localStorage.setItem('favoritos', JSON.stringify(favoritos));
+renderizarFavoritos();
 };
+
+//Renderizar favoritos
+function renderizarFavoritos() {
+    ListaFavoritos.innerHTML = '';
+
+    if (favoritos.length === 0) {
+    ListaFavoritos.innerHTML = '<p>No hay productos favoritos.</p>';
+
+    favCount.textContent = 0;
+    favCount.classList.remove('active');
+    return;
+} 
+favoritos.forEach(id => {
+    const prod = productosHtech.find(producto => producto.id === id);
+    if (!prod) return;
+
+    const li = document.createElement('li');
+    li.className = 'item-fav';
+    li.innerHTML = `
+    <img src="${prod.img}" alt="portada del producto ${prod.titulo}">
+    <span>${prod.titulo}</span>
+    <button class="remove-item" data-id="${id}">
+    <span class="sr-only">Eliminar de favoritos</span>
+  </button>
+`
+ListaFavoritos.appendChild(li);
+});
+
+favCount.textContent = favoritos.length;
+favCount.classList.add('active');
+}
+
+ListaFavoritos.addEventListener('click', (e) => {
+const btn = e.target.closest('.remove-item');
+if  (btn){
+    const id = Number(btn.dataset.id);
+    favoritos = favoritos.filter(favID => favID !== id);
+    localStorage.setItem('favoritos', JSON.stringify(favoritos));
+    
+    renderizarFavoritos();
+    renderizarCatalogo();
+}
+});
 
 
 document.addEventListener('DOMContentLoaded', async() => {
--- JS/menu.js
+++ JS/menu.js
@@ -1,10 +1,26 @@
+//menu principal
 const primaryNav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".menu-btn");
+
+//capa oscura
 const CloseAll = document.querySelector(".fantasma");
 
+//menu favoritos
+const primaryFav = document.querySelector('.primary-fav');
+const favToggle = document.querySelector('.fav-btn');
+const closeFav = document.querySelector('.fav-cerrar');
 
+
+
+//menu carrito
+const primaryCart = document.querySelector('.primary-cart');
+const cartToggle = document.querySelector('.cart-btn');
+const closeCart = document.querySelector('.cart-cerrar');
+
+
+
+//Menu Principal
 navToggle.addEventListener("click", () => {
-   
     const visibility = primaryNav.getAttribute("data-visible");
     
   
@@ -20,14 +36,70 @@ navToggle.addEventListener("click", () => {
 
 });
 
-CloseAll.addEventListener ('click', () => {
-    const visibility = primaryNav.getAttribute('data-visible');
 
-if (visibility === "true") {
-        primaryNav.setAttribute("data-visible", "false");
-        navToggle.setAttribute("aria-expanded", "fasle");
-        CloseAll.setAttribute("data-visible", "false");
+
+//favoritos
+favToggle.addEventListener('click', () => {
+    const visibility = primaryFav.getAttribute('data-visible');
+if(visibility === 'false'){
+   primaryFav.setAttribute('data-visible', 'true');
+  CloseAll.setAttribute('data-visible', 'true');
+}else if(visibility === 'true'){
+primaryFav.setAttribute('data-visible', 'false');
+CloseAll.setAttribute('data-visible', 'false');
+
+}
+})
+//cerrar favoritos
+closeFav.addEventListener('click', () => {
+    const visibility = primaryFav.getAttribute('data-visible');
     
+    if (visibility === 'true') {
+        primaryFav.setAttribute('data-visible', 'false');
+        CloseAll.setAttribute('data-visible', 'false');
     }
-
 });
+
+
+///carrito
+cartToggle.addEventListener('click', () => {
+    const visibility = primaryCart.getAttribute('data-visible');
+    
+    if (visibility === 'false') {
+        primaryCart.setAttribute('data-visible', 'true');
+        CloseAll.setAttribute('data-visible', 'true');
+    } else if (visibility === 'true') {
+        primaryCart.setAttribute('data-visible', 'false');
+        CloseAll.setAttribute('data-visible', 'false');
+    }
+});
+
+//cerrar carrito
+closeCart.addEventListener('click', () => {
+    const visibility = primaryCart.getAttribute('data-visible');
+    
+    if (visibility === 'true') {
+        primaryCart.setAttribute('data-visible', 'false');
+        CloseAll.setAttribute('data-visible', 'false');
+    }
+});
+
+
+//cerrar todo con el fantasma
+CloseAll.addEventListener('click', () => {
+    const visibilityNav = primaryNav.getAttribute('data-visible');
+    const visibilityFav = primaryFav.getAttribute('data-visible');
+    const visibilityCart = primaryCart.getAttribute('data-visible');
+
+    if(visibilityNav === 'true' || visibilityFav === 'true' || visibilityCart === 'true'){
+        primaryNav.setAttribute('data-visible', 'false');
+        primaryFav.setAttribute('data-visible', 'false');
+        primaryCart.setAttribute('data-visible', 'false');
+        
+        navToggle.setAttribute('aria-expanded', 'false');
+        CloseAll.setAttribute('data-visible', 'false');
+    }
+});
+
+
+
